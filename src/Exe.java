0c9a9d59d192a95a30d28806d4e8751a4d65972d
2222222222222412 <85693353+2222222222222412@users.noreply.github.com>
2021-12-02 18:56:35 +0530

    JDBC

---
+++ src/Exe.java
@@ -0,0 +1,72 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Exe
+{
+    public static void main(String[] args) throws ClassNotFoundException,SQLException  {
+        Scanner sc = new Scanner(System.in);
+
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3308/e2_data", "root", "");
+        PreparedStatement s = con.prepareStatement("insert into info values(?,?)");
+        try {
+
+
+            String  str;
+            str = sc.nextLine();
+            Pattern p=Pattern.compile("(0|[+]91)?[96][0-9]{9}");
+            Pattern k=Pattern.compile("[a-zA-Z0-9._]+@gmail.com");
+            Matcher m=p.matcher(str);
+            Matcher neer = k.matcher(str);
+            String val1 =null;
+            String val2 =null;
+            int ctr=0;
+            if(m.find())
+            {
+
+
+                val1 = m.group();
+                ctr++;
+
+
+            }
+            if ( neer.find())
+            {
+
+                val2 = neer.group();
+                ctr++;
+            }
+            if( val1.equals("") || val2.equals("") || ctr<2)
+            {
+                throw new Exception("MY CUSTOM ONE IN PHONE NUMBER");
+            }
+            else
+            {
+                s.setString(1,val1);
+                s.setString(2,val2);
+                s.executeUpdate();
+            }
+
+
+
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println(e);
+
+        }
+
+
+
+
+    }
+
+
+}
